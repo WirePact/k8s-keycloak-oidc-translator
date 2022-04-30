c370ceb82ff8d45db27f56892487d9f3383b8a22
Christoph Bühler <buehler@users.noreply.github.com>
2022-04-30 13:31:34 +0200

    feat: rust implementation and new PKI (#11)
    This upgrades the basic auth translator to the new
    PKI (gRPC) version 2.0.0.
    
    BREAKING CHANGE: This removes the usage of
    http requests to the new format of gRPC communication
    between PKI and translator.
    
    BREAKING CHANGE: Rename some of the options
    due to the fact that rust "clap" default names
    some cli options.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,41 @@
-FROM golang:1.16-alpine as build
+FROM rust:1.60-alpine as build
+
+ARG BUILD_VERSION
+
+RUN apk add --update --no-cache openssl-dev musl-dev protoc
+RUN rustup component add rustfmt
 
 WORKDIR /app
 
-ENV GOOS=linux \
-    GOARCH=amd64 \
-    USER=appuser \
-    UID=1000
+COPY . .
+
+ENV RUSTFLAGS="-C target-feature=-crt-static"
+RUN sed -i -e "s/^version = .*/version = \"${BUILD_VERSION}\"/" Cargo.toml
+RUN cargo install --path .
+
+FROM alpine:3.15
+
+ARG BUILD_VERSION
+ARG COMMIT_SHA
+
+LABEL org.opencontainers.image.source="https://github.com/WirePact/k8s-token-exchange-translator" \
+    org.opencontainers.image.authors="cbuehler@rootd.ch" \
+    org.opencontainers.image.url="https://github.com/WirePact/k8s-token-exchange-translator" \
+    org.opencontainers.image.documentation="https://github.com/WirePact/k8s-token-exchange-translator/blob/main/README.md" \
+    org.opencontainers.image.source="https://github.com/WirePact/k8s-token-exchange-translator/blob/main/Dockerfile" \
+    org.opencontainers.image.version="${BUILD_VERSION}" \
+    org.opencontainers.image.revision="${COMMIT_SHA}" \
+    org.opencontainers.image.licenses="Apache-2.0" \
+    org.opencontainers.image.title="WirePact Kubernetes Token Exchange Translator" \
+    org.opencontainers.image.description="Translator for WirePact that handles Bearer Token Auth via Token Exchange (RFC8693) for any software behind."
+
+WORKDIR /app
+
+ENV USER=appuser \
+    UID=1000 \
+    BUILD_VERSION=${BUILD_VERSION}
+
+COPY --from=build /usr/local/cargo/bin/k8s-token-exchange-translator /usr/local/bin/k8s-token-exchange-translator
 
 RUN adduser \
     --disabled-password \
@@ -14,41 +44,11 @@ RUN adduser \
     --shell "/sbin/nologin" \
     --no-create-home \
     --uid "${UID}" \
-    "${USER}"
-
-COPY go.mod go.sum ./
-RUN go mod download && go mod verify
-
-COPY . .
-
-RUN go build -ldflags="-w -s" -o /go/bin/app
-
-
-FROM alpine
-
-ARG BUILD_VERSION
-ARG COMMIT_SHA
-
-LABEL org.opencontainers.image.source="https://github.com/WirePact/k8s-keycloak-oidc-translator" \
-    org.opencontainers.image.authors="cbuehler@rootd.ch" \
-    org.opencontainers.image.url="https://github.com/WirePact/k8s-keycloak-oidc-translator" \
-    org.opencontainers.image.documentation="https://github.com/WirePact/k8s-keycloak-oidc-translator/blob/main/README.md" \
-    org.opencontainers.image.source="https://github.com/WirePact/k8s-keycloak-oidc-translator/blob/main/Dockerfile" \
-    org.opencontainers.image.version="${BUILD_VERSION}" \
-    org.opencontainers.image.revision="${COMMIT_SHA}" \
-    org.opencontainers.image.licenses="Apache-2.0" \
-    org.opencontainers.image.title="WirePact Kubernetes Keycloak OIDC Translator" \
-    org.opencontainers.image.description="Translator for WirePact that handles OIDC authentication for a Keycloak instance for any software behind."
-
-WORKDIR /app
-
-ENV BUILD_VERSION=${BUILD_VERSION}
-
-COPY --from=build /etc/passwd /etc/group /etc/
-COPY --from=build /go/bin/app /app/app
-
-RUN chown -R appuser:appuser /app
+    "${USER}" && \
+    chown -R appuser:appuser /app && \
+    chmod +x /usr/local/bin/k8s-token-exchange-translator && \
+    apk add --update --no-cache libgcc ca-certificates
 
 USER appuser:appuser
 
-ENTRYPOINT ["/app/app"]
+ENTRYPOINT ["/usr/local/bin/k8s-token-exchange-translator"]
---
+++ src/main.rs
@@ -0,0 +1,219 @@
+use std::sync::Arc;
+
+use clap::{ArgEnum, Parser};
+use log::{debug, error, info};
+use tokio::sync::Mutex;
+use wirepact_translator::{
+    run_translator, CheckRequest, EgressResult, IngressResult, Status, Translator,
+    TranslatorConfig, HTTP_AUTHORIZATION_HEADER,
+};
+
+use crate::provider::{ClientCredentialProvider, Provider};
+
+mod provider;
+
+#[derive(Clone, Debug, ArgEnum)]
+enum AuthType {
+    ClientCredentials,
+    JWTProfile,
+}
+
+#[derive(Parser, Debug)]
+#[clap(version, about, long_about = None)]
+struct Cli {
+    /// The address of the WirePact PKI.
+    #[clap(short, long, env)]
+    pki_address: String,
+
+    /// The name of the translator. This is used as common name
+    /// when requesting a certificate from the PKI.
+    #[clap(short, long, env, default_value = "k8s oidc token exchange translator")]
+    name: String,
+
+    /// The port that the server will listen for
+    /// ingress communication (incoming connections) on.
+    #[clap(short, long, env, default_value = "50051")]
+    ingress_port: u16,
+
+    /// The port that the server will listen for
+    /// egress communication (outgoing connections) on.
+    #[clap(short, long, env, default_value = "50052")]
+    egress_port: u16,
+
+    /// If set, debug log messages are printed as well.
+    #[clap(short, long, env)]
+    debug: bool,
+
+    /// The issuer for OIDC tokens. This is used in conjunction
+    /// with the token-exchange grant to fetch an access token
+    /// on the users behalf. If `discovery_url` is not set, this
+    /// url will be used with `/.well-known/openid-configuration`
+    /// to fetch the endpoint urls.
+    #[clap(long, env)]
+    issuer: String,
+
+    /// Optional overwrite of the well-known discovery document endpoint
+    /// of the issuer. If set, please provide the full url to the OIDC
+    /// discovery document.
+    #[clap(long, env)]
+    discovery_url: Option<String>,
+
+    /// Determine the authentication type for the issuer. There exist
+    /// two authentication types:
+    ///
+    /// - Client Credentials: The translator will use client ID and
+    ///   client secret to authenticate itself against the issuer.
+    ///
+    /// - JWT Profile: The translator will use a JWT profile
+    ///   ([RFC7523](https://datatracker.ietf.org/doc/html/rfc7523)) to authenticate
+    ///   itself against the issuer. (not implemented yet).
+    ///
+    /// Depending on the selected auth type, other parameters are required.
+    #[clap(arg_enum, long, env, default_value = "client-credentials")]
+    auth_type: AuthType,
+
+    /// Required if auth_type is set to [AuthType::ClientCredentials].
+    /// Defines the client ID to use when authenticating against the issuer.
+    #[clap(long, env)]
+    client_id: Option<String>,
+
+    /// Required if auth_type is set to [AuthType::ClientCredentials].
+    /// Defines the client secret to use when authenticating against the issuer.
+    #[clap(long, env)]
+    client_secret: Option<String>,
+    // /// Required if auth_type is set to [AuthType::JWTProfile].
+    // /// Defines the file path to the JWT profile to use when authenticating against the issuer.
+    // /// The profile must be a JSON file containing the following fields:
+    // ///
+    // /// - userId: The user ID of the machine account.
+    // ///
+    // /// - keyId: The ID of the used signing RSA key.
+    // ///
+    // /// - key: The pem encoded RSA (private) key.
+    // #[clap(long, env)]
+    // jwt_profile_path: Option<String>,
+    // // TODO: token endpoint auth type must be specified (client secret basic auth, client secret post, private key jwt)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+
+    let level = match cli.debug {
+        true => log::LevelFilter::Debug,
+        false => log::LevelFilter::Info,
+    };
+
+    env_logger::builder()
+        .filter_module("k8s_token_exchange_translator", level)
+        .filter_module("wirepact_translator", level)
+        .init();
+
+    info!("Starting oidc token exchange translator '{}'.", cli.name);
+    debug!("Debug logging is enabled.");
+
+    let url = match cli.discovery_url {
+        Some(url) => url,
+        None => format!("{}/.well-known/openid-configuration", cli.issuer),
+    };
+
+    let authenticator: Arc<Mutex<dyn Provider>>;
+
+    match cli.auth_type {
+        AuthType::JWTProfile => {
+            unimplemented!("JWT Profile authentication is not implemented yet.")
+        }
+        AuthType::ClientCredentials => {
+            let client_id = cli
+                .client_id
+                .expect("client_id is required for client credentials authentication.");
+            let client_secret = cli
+                .client_secret
+                .expect("client_secret is required for client credentials authentication.");
+            authenticator = Arc::new(Mutex::new(
+                ClientCredentialProvider::new(&url, client_id, client_secret).await?,
+            ));
+        }
+    }
+
+    run_translator(&TranslatorConfig {
+        pki_address: cli.pki_address,
+        common_name: cli.name,
+        ingress_port: cli.ingress_port,
+        egress_port: cli.egress_port,
+        translator: Arc::new(OidcTranslator { authenticator }),
+    })
+    .await?;
+
+    Ok(())
+}
+
+struct OidcTranslator {
+    authenticator: Arc<Mutex<dyn Provider>>,
+}
+
+#[wirepact_translator::async_trait]
+impl Translator for OidcTranslator {
+    async fn ingress(&self, subject_id: &str, _: &CheckRequest) -> Result<IngressResult, Status> {
+        let access_token = self
+            .authenticator
+            .lock()
+            .await
+            .access_token_for_user_id(subject_id)
+            .await
+            .map_err(|e| {
+                error!(
+                    "Failed to get access token for user ID '{}': {}",
+                    subject_id, e
+                );
+                Status::internal(format!(
+                    "Failed to get access token for user ID '{}': {}",
+                    subject_id, e
+                ))
+            })?;
+
+        debug!("Fetched access token for user with ID '{}'.", subject_id);
+
+        Ok(IngressResult::allowed(
+            Some(vec![(
+                HTTP_AUTHORIZATION_HEADER.to_string(),
+                format!("Bearer {}", access_token),
+            )]),
+            None,
+        ))
+    }
+
+    async fn egress(&self, request: &CheckRequest) -> Result<EgressResult, Status> {
+        let auth_header = self.get_header(request, HTTP_AUTHORIZATION_HEADER)?;
+
+        if auth_header.is_none() {
+            debug!("No authorization header found. Skip request.");
+            return Ok(EgressResult::skip());
+        }
+
+        let auth_header = auth_header.unwrap();
+        if !auth_header.starts_with("Bearer ") {
+            debug!("Authorization header does not start with 'Bearer'. Skip Request.");
+            return Ok(EgressResult::skip());
+        }
+
+        let access_token = auth_header.replace("Bearer ", "");
+        let subject = self
+            .authenticator
+            .lock()
+            .await
+            .user_id_for_token(&access_token)
+            .await
+            .map_err(|e| {
+                error!("Failed to get user ID for access token: {}", e);
+                Status::internal(format!("Failed to get user ID for access token: {}", e))
+            })?;
+
+        debug!("Fetched user ID '{}' from access token.", subject);
+
+        Ok(EgressResult::allowed(
+            subject,
+            Some(vec![HTTP_AUTHORIZATION_HEADER.to_string()]),
+        ))
+    }
+}
